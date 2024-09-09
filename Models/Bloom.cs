4553ad8d7dc5648e95f3f5fa5d4affcf2ac29aaf
Mike B <mikeb999999@gmail.com>
2024-09-09 14:23:31 +0100

    Changes to bloom model

--- Models/Bloom.cs
+++ Models/Bloom.cs
@@ -1,4 +1,14 @@
-﻿namespace FlowerzAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlowerzAPI.Models
 {
-    public record Bloom (int Id, string Name, string Description);
+    public record Bloom(
+        int Id,
+
+        [Required]
+        [StringLength(100)]
+        string Name,
+
+        [StringLength(100)]
+        string? Description);
 }
